191171f425776b594717de7dd1891f14edb08b8e
juunone <juunone@gmail.com>
2019-08-08 17:24:58 +0900

    Added globalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,29 @@ import { Link } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 import { rhythm, scale } from "../utils/typography";
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { Paragraph, SocialLink, Wrapper } from './styled'
 
 class Layout extends React.Component {
   render() {
     const { location, title, children, social } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
+    // Define what props.theme will look like
+    const theme = {
+      fontFamily: "'Nanum Gothic', sans-serif;"
+    };
+
+    const GlobalStyle = createGlobalStyle`
+      body {
+        font-family: ${props => props.theme.fontFamily};
+      }
+
+      ul{
+        margin-top:1em;
+        list-style-position:initial;
+        padding-left:1em;
+      }
+    `
 
     let header
 
@@ -55,34 +72,37 @@ class Layout extends React.Component {
       )
     }
     return (
-      <Wrapper>
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          <Paragraph>
-            <SocialLink as="a" href={`https://github.com/${social.github}`} title="Github" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faGithub} />
-            </SocialLink>
-            <SocialLink as="a" href={`https://www.linkedin.com/in/${social.linkedin}`} title="Linkedin" tSocialLinkrget="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </SocialLink>
-            <SocialLink as="a" href={`https://twitter.com/${social.twitter}`} title="Twitter" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitterSquare} />
-            </SocialLink>
-          </Paragraph>
-          <Paragraph>
-            <Link style={{ boxShadow: `none` }} to='/about'>
-              About
-            </Link>
-          </Paragraph>
-          <Paragraph>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a><br/>
-            Was made with &#x1F49A; by 최 준원
-          </Paragraph>
-        </footer>
-      </Wrapper>
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <GlobalStyle />
+          <header>{header}</header>
+          <main>{children}</main>
+          <footer>
+            <Paragraph>
+              <SocialLink as="a" href={`https://github.com/${social.github}`} title="Github" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} />
+              </SocialLink>
+              <SocialLink as="a" href={`https://www.linkedin.com/in/${social.linkedin}`} title="Linkedin" tSocialLinkrget="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </SocialLink>
+              <SocialLink as="a" href={`https://twitter.com/${social.twitter}`} title="Twitter" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitterSquare} />
+              </SocialLink>
+            </Paragraph>
+            <Paragraph>
+              <Link style={{ boxShadow: `none` }} to='/about'>
+                About
+              </Link>
+            </Paragraph>
+            <Paragraph>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a><br/>
+              Was made with &#x1F49A; by 최 준원
+            </Paragraph>
+          </footer>
+        </Wrapper>
+      </ThemeProvider>
     )
   }
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,42 +2,34 @@ import React from 'react';
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { ThemeProvider } from 'styled-components';
 import { Wrapper, Header1, Ul } from '../components/styled';
 
 const about = (props) =>{
   const { data } = props;
   const siteTitle = data.site.siteMetadata.title;
   const social = data.site.siteMetadata.social;
-  const theme = {
-    fontFamily: "'Nanum Gothic', sans-serif;"
-  };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <Layout location={props.location} title={siteTitle} social={social}>
-          <SEO title="about" />
-            <Header1>최 준원 (Juunone)</Header1>
+    <Layout location={props.location} title={siteTitle} social={social}>
+      <SEO title="about" />
+        <Header1>최 준원 (Juunone)</Header1>
 
-            <span role="img" aria-label="email">&#x1F4E7;</span> juunone@gmail.com <br />
-            <span role="img" aria-label="github">&#x1F4BB;</span> https://github.com/juunone <br />
-            <span role="img" aria-label="linkedin">&#x1F606;</span> https://www.linkedin.com/in/juunone/ <br />
-            <span role="img" aria-label="blog">&#x1F310;</span> https://juunone.github.io/ <br />
+        <span role="img" aria-label="email">&#x1F4E7;</span> juunone@gmail.com <br />
+        <span role="img" aria-label="github">&#x1F4BB;</span> https://github.com/juunone <br />
+        <span role="img" aria-label="linkedin">&#x1F606;</span> https://www.linkedin.com/in/juunone/ <br />
+        <span role="img" aria-label="blog">&#x1F310;</span> https://juunone.github.io/ <br />
 
-            <Header1><span role="img" aria-label="introduction">&#x1F680;</span> 소개</Header1>
+        <Header1><span role="img" aria-label="introduction">&#x1F680;</span> 소개</Header1>
 
-            안녕하세요.<br />
-            클라이언트 웹 어플리케이션 개발자 최준원 입니다.<br />
-            경험과 상호작용을 통해 고민하고 개발합니다.
-            <Ul>
-              <li>다양한 웹 기술을 이용한 클라이언트 웹 어플리케이션을 만듭니다.</li>
-              <li>새로운 기술을 공부하는 것을 좋아합니다.</li>
-              <li>타인과의 커뮤니케이션 및 대화는 환영합니다.</li>
-            </Ul>
-        </Layout>
-      </Wrapper>
-    </ThemeProvider>
+        안녕하세요.<br />
+        클라이언트 웹 어플리케이션 개발자 최준원 입니다.<br />
+        경험과 상호작용을 통해 고민하고 개발합니다.
+        <ul>
+          <li>다양한 웹 기술을 이용한 클라이언트 웹 어플리케이션을 만듭니다.</li>
+          <li>새로운 기술을 공부하는 것을 좋아합니다.</li>
+          <li>타인과의 커뮤니케이션 및 대화는 환영합니다.</li>
+        </ul>
+    </Layout>
   )
 };
 
